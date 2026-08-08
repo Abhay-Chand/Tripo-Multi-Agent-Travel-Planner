161b52dcfa208615f8e874a3fa2cabe46600cf92
Abhay-Chand <chandabhay639@gmail.com>
2026-08-09 00:19:12 +0530

    update backend.py

--- test.py
+++ test.py
@@ -1,9 +1,25 @@
 # to test our files
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
+
+
+
+# Test -01
 # res = tavily_search("Best hotels in India")
 # print(res)
 
+# Test -02
+# res = search_flights("Plan a 7 days Dubai trip from India" )
+# print(res)
 
-res = search_flights("Plan a 7 days Dubai trip from India" )
-print(res)
+# Test - -03
+user_input = input("Enter travel support : ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
+
+print("\n FINAL RESPONSE : \n")
+print(response["answer"])
